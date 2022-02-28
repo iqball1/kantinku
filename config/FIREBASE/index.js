097a6a20e72a81ab5d4f9cc2db6b6b60fd8ce652
iqball1 <100454265+iqball1@users.noreply.github.com>
2022-02-28 12:15:00 +0700

    Add files via upload

---
+++ config/FIREBASE/index.js
@@ -0,0 +1,49 @@
+import firebase from "firebase";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBfEOsB8f4Gy-WorsKHjYIgzvDN_DUQH-0",
+    authDomain: "kantinku-2f934.firebaseapp.com",
+    databaseURL: "https://kantinku-2f934-default-rtdb.firebaseio.com",
+    projectId: "kantinku-2f934",
+    storageBucket: "kantinku-2f934.appspot.com",
+    messagingSenderId: "480440407229",
+    appId: "1:480440407229:web:cae77b5315ff27d54b13d4"
+});
+const FIREBASE = firebase;
+
+export default FIREBASE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBfEOsB8f4Gy-WorsKHjYIgzvDN_DUQH-0",
+//   authDomain: "kantinku-2f934.firebaseapp.com",
+//   databaseURL: "https://kantinku-2f934-default-rtdb.firebaseio.com",
+//   projectId: "kantinku-2f934",
+//   storageBucket: "kantinku-2f934.appspot.com",
+//   messagingSenderId: "480440407229",
+//   appId: "1:480440407229:web:cae77b5315ff27d54b13d4"
+// };
+
+// // Initialize Firebase
+// const FIREBASE = initializeApp(firebaseConfig);
+
+// export default FIREBASE;
